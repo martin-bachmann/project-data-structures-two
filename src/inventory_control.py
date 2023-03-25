fd70229bf3abf4404ed2721b8b30d57a6a173c50
Martin Bachmann <martinpezbach@gmail.com>
2023-03-25 17:24:31 -0300

    feat: implement inventory control functions

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -30,8 +30,33 @@ class InventoryControl:
     def add_new_order(self, customer, order, day):
         receita = self.INGREDIENTS[order]
         for ingredient in receita:
+            if (
+                self.ingredientes_utilizados[ingredient]
+                >= self.MINIMUM_INVENTORY[ingredient]
+            ):
+                return False
             self.ingredientes_utilizados[ingredient] += 1
         self._orders.append((customer, order, day))
 
     def get_quantities_to_buy(self):
         return self.ingredientes_utilizados
+
+    def get_unavailable_ingredients(self):
+        ingredientes_zerados = set()
+
+        for ingrediente in self.ingredientes_utilizados.items():
+            if ingrediente[1] >= self.MINIMUM_INVENTORY[ingrediente[0]]:
+                ingredientes_zerados.add(ingrediente[0])
+
+        return ingredientes_zerados
+
+    def get_available_dishes(self):
+        receitas_possiveis = set(self.INGREDIENTS.keys())
+        ingredientes_zerados = self.get_unavailable_ingredients()
+
+        for receita in self.INGREDIENTS.items():
+            for ingrediente in receita[1]:
+                if ingrediente in ingredientes_zerados:
+                    receitas_possiveis.discard(receita[0])
+
+        return receitas_possiveis
