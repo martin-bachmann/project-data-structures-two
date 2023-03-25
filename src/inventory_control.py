405c3068922118b1402b3b71e47ab8d5d1a06be7
Martin Bachmann <martinpezbach@gmail.com>
2023-03-25 17:10:07 -0300

    feat: implement inventory control functions

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,37 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self._orders = list()
+        self.ingredientes_utilizados = {
+            "pao": 0,
+            "carne": 0,
+            "queijo": 0,
+            "molho": 0,
+            "presunto": 0,
+            "massa": 0,
+            "frango": 0,
+        }
 
     def add_new_order(self, customer, order, day):
-        pass
+        receita = self.INGREDIENTS[order]
+        for ingredient in receita:
+            self.ingredientes_utilizados[ingredient] += 1
+        self._orders.append((customer, order, day))
 
     def get_quantities_to_buy(self):
-        pass
+        return self.ingredientes_utilizados
